ee5c91aad67df76a7bc11bafbf4fe59925ed4129
sam <sam@smjdesign.co.uk>
2020-10-24 20:45:01 +0100

    Updates

--- DocumentManager.Common/Queries/GetDocumentQuery.cs
+++ DocumentManager.Common/Queries/GetDocumentQuery.cs
@@ -25,9 +25,7 @@ namespace DocumentManager.Core.Queries
             _repository = repository;
         }
 
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task<ValueWrapper<Document>> Handle(GetDocumentQuery request, CancellationToken cancellationToken)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             var document = _repository.GetSingle(request.Filename);
 
